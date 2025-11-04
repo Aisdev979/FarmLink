6c855171da92a7ba14474a00d7ec0c8ffc91fe78
Ogbebor Aisosa <aisdevinstitute@gmail.com>
2025-11-04 04:50:05 +0100

    Responsiveness improvement

--- frontend/Js/nav.js
+++ frontend/Js/nav.js
@@ -2,4 +2,4 @@ const openBtn = document.querySelector('.open-menu');
 const navMenu = document.querySelector('.nav-header');
 
 openBtn.addEventListener('click', () => navMenu.style.right = '0');
-navMenu.addEventListener('click', () => navMenu.style.right = '-30em');
+navMenu.addEventListener('click', () => navMenu.style.right = '-60em');
